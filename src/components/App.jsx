9195de4a7053635085b6e1b0ea621afccc8ae6f4
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-12-01 15:16:48 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import LogoutConfirm from '../components/ModalLogout/LogoutConfirm';
+import OpenTransactionBtn from '../components/addTransactionBtn/addTransactionBtn';
 
 export const App = () => {
   return (
     <>
+      <OpenTransactionBtn />
       <LogoutConfirm />
     </>
   );
